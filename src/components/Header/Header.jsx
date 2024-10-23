5099f9e3363fbba12159fa695fdf6412b46282a3
Akash4056 <akashsaha1313@gmail.com>
2024-10-23 20:42:29 +0530

    Spelling Fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
     return (
         <div className="my-10">
-            <h1 className='text-center text-3xl md:text-4xl lg:text-5xl font-bold'>Memorible Water Bottles</h1>
+            <h1 className='text-center text-3xl md:text-4xl lg:text-5xl font-bold'>Memorable Water Bottles</h1>
         </div>
     )
 }
